dce7fce47782c31755ebcdd31f092c0345a8e669
mxnueh <enmanuelnunezmatias@gmail.com>
2026-03-18 23:36:28 -0400

    feat: implement cart functionality and update guitar component to handle add to cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { db } from "./data/db"
 
 function App() {
     const [guitars, setGuitars] = useState(db)  
+    const [cart, setCart] = useState([])
 
     // Buena practica para cargar datos desde una API o base de datos, 
     // pero como el arreglo ya esta importado, no es necesario usarlo
@@ -13,18 +14,48 @@ function App() {
     // //     setGuitars(db)
     // // }, [])
 
+    // LO DEL MAP
+    // En este map se renderiza un componente Guitar por cada elemento del
+    // arreglo guitars, pero como no se esta usando el elemento del arreglo,
+    // se puede omitir y solo usar el map para renderizar el componente Guitar
+    // la cantidad de veces que sea necesario, en este caso 5 veces,
+    // ya que el arreglo tiene 5 elementos.
+
+    // LOS PROPS
+    // Los props son una forma de pasar datos desde un componente padre a un componente hijo,
+    // en este caso, se pueden pasar los datos de cada guitarra como props al componente Guitar,
+    // para que el componente Guitar pueda renderizar la información de cada guitarra.
+
+    function addToCart(item) {
+        const itemExists = cart.findIndex(cartItem => cartItem.id === item.id);
+        
+        if (itemExists >= 0) { //Existe en el carrito
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++; // Incrementa la cantidad
+            setCart(updatedCart);
+        } else {
+            item.quantity = 1; // Agrega la propiedad quantity al item
+            setCart([...cart, item])
+        }
+    }
+
     return (
     <>
     <Header />
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
+        
         <div className="row mt-5">
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
+            {guitars.map((guitar) => (
+                <Guitar 
+                key={guitar.id}
+                guitar={guitar}
+                // cart={cart}
+                addToCart={addToCart}
+                setCart={setCart}
+                />
+            ))}
         </div>
     </main>
 
